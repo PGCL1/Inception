4e130bd350f46d771b63ebe73bbb4016daef9a03
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2025-02-03 18:27:13 +0100

    problem with mariadb config

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -2,6 +2,8 @@
 
 # create directories mariadb
 mkdir -p /var/lib/mysql /run/mysqld 
+touch /var/run/mysqld/mysqld.sock
+touch /var/run/mysqld/mysqld.pid
 
 if [ ! -d /var/lib/mysql ] && [ ! -d /run/mysqld ]; then
 		echo "Directories were not correctly created"
@@ -10,6 +12,9 @@ fi
 
 # change ownership to mysql user and group
 chown -R mysql:mysql /var/lib/mysql /run/mysqld
+chown -R mysql:mysql /var/run/mysqld/mysqld.sock
+chown -R mysql:mysql /var/run/mysqld/mysqld.pid
+chmod -R 755 /var/run/mysqld/mysqld.sock
 
 # set working dir
 cd /var/lib/mysql
