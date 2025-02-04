bb5bd51c1cb93b2a0be5f9f9d2bab20d2e3a3aaf
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2025-02-04 19:56:43 +0100

    modified DB_HOST

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -15,7 +15,7 @@ if [ ! -f wp-config.php ]; then
       --dbname="$MYSQL_DATABASE" \
 	  --dbuser="$MYSQL_ROOT_USER" \ 
 	  --dbpass="$MYSQL_ROOT_PASSWORD" \
-	  --dbhost="$MYSQL_HOST"
+	  --dbhost="mariadb"
 
   echo "Installing WordPress..."
   wp core install --allow-root --path=/var/www/html \
