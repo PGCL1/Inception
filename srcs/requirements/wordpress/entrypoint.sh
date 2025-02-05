35de794edf3251512956a3c80ba997de57ddcc10
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2025-02-05 19:14:12 +0100

    fixed mariadb conf, problem with wordpress creating tables

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,24 +1,26 @@
-#!/bin/bash
-set -e
+#!/bin/sh
 
-mkdir -p /var/www/html
-cd /var/www/html
 mkdir -p /var/www/wordpress && chmod -R 755 /var/www/wordpress
+
+# wait for mariadb to start
+sleep 5
 
 # Only install WordPress if not already installed.
 if [ ! -f wp-config.php ]; then
   echo "Downloading WordPress core..."
-  wp core download --allow-root --path=/var/www/html
+  wp core download --allow-root --path=/var/www/wordpress
+
 
   echo "Creating wp-config.php..."
-  wp config create --allow-root --path=/var/www/html \
-      --dbname="$MYSQL_DATABASE" \
-	  --dbuser="$MYSQL_ROOT_USER" \ 
-	  --dbpass="$MYSQL_ROOT_PASSWORD" \
-	  --dbhost="mariadb"
+  wp config create --path=/var/www/wordpress \
+      --dbname="$WP_DATABASE" \
+	  --dbuser="$WP_ADMIN_USER" \ 
+	  --dbpass="$WP_ADMIN_PASSWORD" \
+	  --dbhost="mariadb" --allow-root
+
 
   echo "Installing WordPress..."
-  wp core install --allow-root --path=/var/www/html \
+  wp core install --allow-root --path=/var/www/wordpress \
       --url="$WP_URL" \
 	  --title="$WP_TITLE" \
       --admin_user="$WP_ADMIN_USER" \
@@ -28,6 +30,8 @@ else
   echo "WordPress already configured. Skipping installation."
 fi
 
+		  sleep infinity
+	wp user create $WP_USER $WP_EMAIL --role='editor' --user_pass=$WP_PASSWORD
+
 # Pass control to the CMD (PHP built-in server, for example)
 exec "$@"
-
